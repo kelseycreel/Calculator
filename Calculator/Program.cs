81b6388d3d1f6bfe5d0330a02299c8cc4e3570d8
Kelsey Creel <kelseycreel@gmail.com>
2020-02-25 21:23:56 -0600

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hyello! I hope you're ready to math today! Enter in comma separated numbers to multiply:");
+            var input = Console.ReadLine();
+            var numbers = new string[input.Length];
+            var sum = 0;
+            if (input.Contains(" "))
+            {
+                numbers = input.Split(" ");
+            }
+            else if (input.Contains(", "))
+            {
+                numbers = input.Split(", ");
+            }
+            else
+            {
+                numbers = input.Split(",");
+            }
+            foreach (var num in numbers)
+            {
+                sum *= int.Parse($"{num}");
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
